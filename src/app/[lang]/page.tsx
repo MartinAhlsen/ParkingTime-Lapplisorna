8fd603b3f1ed8639212df53e3e7abbf74a55deb6
Davide Baldi <d.baldi88@gmail.com>
2024-05-29 19:48:11 +0200

    made variables more readable

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,5 +3,5 @@ import { getDictionary } from './dictionaries'
 
 export default async function Home({params: {lang}}) {
   const dict = await getDictionary(lang) // en
-  return <main>{dict.products.intro}</main>;
+  return <main>{dict.home.title}</main>;
 }
