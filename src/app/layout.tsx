c47671a08d0a18f6123d87b30727de39742573a4
MartinAhlsen <martin.ahlsen@gmail.com>
2024-05-30 09:35:40 +0200

    changed nextFonts to px @next/codemod@latest built-in-next-font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Lato, Roboto } from "@next/font/google";
+import { Lato, Roboto } from "next/font/google";
 import "./globals.css";
 
 const lato = Lato({
