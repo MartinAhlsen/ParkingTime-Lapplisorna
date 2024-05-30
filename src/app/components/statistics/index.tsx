b5995abc8ecd7a7813783bbfe4014b3605b33f98
Davide Baldi <d.baldi88@gmail.com>
2024-05-30 08:54:07 +0200

    fixer from darius

---
+++ src/app/components/statistics/index.tsx
@@ -0,0 +1,11 @@
+
+
+const statistics = () => {
+    return (
+        <div>
+          
+        </div>
+    )
+};
+
+export default C:\Users\ASUS\Desktop\FEA23\NEXT\ParkingTime-Lapplisorna\src\app\components\statistics
