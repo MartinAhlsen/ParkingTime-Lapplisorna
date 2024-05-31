50a30ecca35e33fe9fd86490d37f8adfe384d798
Davide Baldi <d.baldi88@gmail.com>
2024-05-31 20:26:53 +0200

    it is workinggit add .git add .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+"use client"
+import { useEffect, useState } from "react";
+import SplashScreen from "./components/SplashScreen/SplashScreen";
+import { AnimatePresence } from "framer-motion";
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default
+      const locomotiveScroll = new LocomotiveScroll()
+      
+      setTimeout(() => {
+        setIsLoading(false)
+        document.body.style.cursor = 'default'
+        window.scrollTo(0, 0)
+      }, 2000)
+    })()
+  }, [])
   return (<main>
+    <AnimatePresence mode="wait">
+          {isLoading && <SplashScreen />}
+        </AnimatePresence>
     <div className="h-96 bg-red-600"></div>
     <div className="h-96 bg-blue-500"></div>
     <div className="h-96 bg-amber-800"></div>
