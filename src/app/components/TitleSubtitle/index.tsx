f67536fb606c09c06b45b4887d37501aeed386f1
Davide Baldi <d.baldi88@gmail.com>
2024-05-31 10:24:19 +0200

    save

---
+++ src/app/components/TitleSubtitle/index.tsx
@@ -0,0 +1,16 @@
+interface TitleSubtitleProps {
+    title: string,
+    subtitle: string | null
+}
+
+const TitleSubtitle = ({title, subtitle}:TitleSubtitleProps) => {
+
+    return (
+        <>
+          {/*title 48/ bold-lato font ecf21 bg                                                                                                       */}
+          {/* colo 2a394e* robot 16 rgular*/}
+        </>
+    )
+};
+
+export default TitleSubtitle
