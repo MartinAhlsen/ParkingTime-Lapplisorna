c564a5226a96db9758e6e32a64c079f3104a4e11
MartinAhlsen <martin.ahlsen@gmail.com>
2024-05-31 13:08:32 +0200

    working button with url handeling and removed erronius images folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,23 @@ import Button from "@/components/Button";
 
 interface ButtonProperties {
   text: string;
-  url?: string;
+  url: string;
   colorTheme: "light" | "dark";
   formCategoryState?: string;
-  buttonSize: "small" | "large";
 }
 
 const TestButton: ButtonProperties = {
   text: "Test",
-  // url?: string;
+  url: "/se/news",
   colorTheme: "dark",
   // formCategoryState?: string;
-  buttonSize: "small",
 };
 
 export default function Home() {
   return (
     <>
       <main>Parking Time</main>
-      <Button
-        text={TestButton.text}
-        colorTheme={TestButton.colorTheme}
-        buttonSize={TestButton.buttonSize}
-      />
+      <Button {...TestButton} />
     </>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,35 +1,34 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 interface ButtonProps {
   text: string;
-  url?: string;
+  url: string;
   colorTheme: "light" | "dark";
   formCategoryState?: string;
-  buttonSize: "small" | "large";
 }
 
-const Button = ({
-  text,
-  url,
-  colorTheme,
-  formCategoryState,
-  buttonSize,
-}: ButtonProps) => {
-  const handleClick = () => {
-    console.log("click");
+const Button = ({ text, url, colorTheme, formCategoryState }: ButtonProps) => {
+  const router = useRouter();
+  const handleClick = (): void => {
+    router.push(url);
   };
   return (
     <button
       onClick={handleClick}
       className={`
-            ${buttonSize === "small" ? "px-3 py-1" : "p-6"} 
             ${
               colorTheme === "dark"
-                ? "bg-black text-white"
-                : "bg-white text-black"
+                ? "bg-pt-secondary text-white"
+                : "bg-white text-pt-secondary"
             }
             rounded-full 
-            button-text
+            button-text 
+            py-[15px]
+            px-[32px]
+            h-[48px]
+            w-[300px]
+            md:w-[160px]
         `}
     >
       {text}
