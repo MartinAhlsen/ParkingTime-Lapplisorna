a90d9a2bb55c71913f8ed101b3259a9050c92014
Davide Baldi <d.baldi88@gmail.com>
2024-05-31 19:53:47 +0200

    working fine more or less

--- src/app/components/Statistics/index.tsx
+++ src/app/components/Statistics/index.tsx
@@ -1,20 +1,59 @@
+"use client"
+
 import StatisticCard from "./StatisticCard";
 import downloadImg from "../../../../public/Images/download-icon.png";
 import mapImg from "../../../../public/Images/map-icon.png";
-import parkingImg from "../../../../public/Images/parking-icon.png"
+import parkingImg from "../../../../public/Images/parking-icon.png";
+import { animate, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Statistics = () => {
-    return (
-        <div className="h-96 bg-pt-background flex flex-col md:flex-row justify-center items-center">
-          <div className="bg-white h-80 rounded-xl mx-1 flex flex-col md:flex-row">
-                <StatisticCard imageLink={downloadImg} number={5000} subtitle="Downloads"/>
-                <div className="basis-1/5"></div>
-                <StatisticCard imageLink={parkingImg} number={4700} subtitle="Parking sessions"/>
-                <div className="basis-1/5"></div>
-                <StatisticCard imageLink={mapImg} number={30} subtitle="Parking zones"/>
-          </div>
-        </div>
-    )
+
+  const count1 = useMotionValue(0);
+  const count2 = useMotionValue(0);
+  const count3 = useMotionValue(0);
+  const rounded1 = useTransform(count1, Math.round);
+  const rounded2 = useTransform(count2, Math.round);
+  const rounded3 = useTransform(count3, Math.round);
+  const [countValue1, setCountValue1] = useState(0);
+  const [countValue2, setCountValue2] = useState(0);
+  const [countValue3, setCountValue3] = useState(0);
+
+  useEffect(() => {
+    // Subscribe to changes in rounded and update the state
+    const unsubscribe1 = rounded1.onChange((latest) => {
+      setCountValue1(latest);
+    });
+    const unsubscribe2 = rounded2.onChange((latest) => {
+      setCountValue2(latest);
+    });
+    const unsubscribe3 = rounded3.onChange((latest) => {
+      setCountValue3(latest);
+    });
+
+    // Start the animation
+    const animation1 = animate(count1, 5000, { duration: 5 });
+    const animation2 = animate(count2, 4700, { duration: 5 });
+    const animation3 = animate(count3, 30, { duration: 3 });
+
+    // Clean up subscription and animation on unmount
+    return () => {
+      unsubscribe1();
+      animation1.stop();
+    };
+  }, [rounded1, count1]);
+
+  return (
+    <div className="h-96 bg-pt-background flex flex-col md:flex-row justify-center items-center">
+      <div className="bg-white h-80 rounded-xl mx-1 flex flex-col md:flex-row">
+        <StatisticCard imageLink={downloadImg} number={countValue1} subtitle="Downloads" />
+        <div className="basis-1/5"></div>
+        <StatisticCard imageLink={parkingImg} number={countValue2} subtitle="Parking sessions" />
+        <div className="basis-1/5"></div>
+        <StatisticCard imageLink={mapImg} number={countValue3} subtitle="Parking zones" />
+      </div>
+    </div>
+  );
 };
 
-export default Statistics
+export default Statistics;
