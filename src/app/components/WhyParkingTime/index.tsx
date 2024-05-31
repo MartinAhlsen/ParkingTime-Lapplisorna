d131e5fc7e6cb53576a7b971f5e959c6aefea5b7
Davide Baldi <d.baldi88@gmail.com>
2024-05-31 12:45:21 +0200

    grid fixed at desktop

--- src/app/components/WhyParkingTime/index.tsx
+++ src/app/components/WhyParkingTime/index.tsx
@@ -11,10 +11,12 @@ const WhyParkingTime = () => {
     return (
         <div className="bg-pt-primary min-h-screen flex flex-col">
             <p className="h3 text-center text-white">Why Parking Time?</p>
+            <div className="bg-pt-primary min-h-screen flex flex-col md:grid md:grid-cols-2">
           <WhyCard imageLink={arrowG} title="Increased revenue & reduced stress" paragraph="Our digital solution attracts more visitors by eliminating physical discs and reducing fines. This simplifies parking, boosts foot traffic, economic activity and city vibrancy  while reducing stress."/>
           <WhyCard imageLink={phone} title="Simplified parking for users" paragraph="Our app lets you effortlessly set, adjust, and monitor parking time. With a user-friendly interface, you can quickly find spots and manage parking. We continuously update based on customer feedback to ensure an efficient and hassle-free experience."/>
-          <WhyCard imageLink={group} title="Integration with existing systems" paragraph="Our digital solution attracts more visitors by eliminating physical discs and reducing fines. This simplifies parking, boosts foot traffic, economic activity and city vibrancy  while reducing stress."/>
+          <WhyCard imageLink={group} title="Integration with existing systems" paragraph="Our system for parking attendants integrates seamlessly with your existing setup. A unified system benefits you by streamlining operations and enhancing the experience for personnel by eliminating the need for two different systems and devices. We make this integration efficient, intuitive, and simple!"/>
           <WhyCard imageLink={computer} title="Data-driven urban planning" paragraph="The digital parking disc can eventually optimize parking resources and urban planning through valuable usage data and behavior patterns, leading to a safer society with better control and access to parking spaces."/>
+          </div>
         </div>
     )
 };
