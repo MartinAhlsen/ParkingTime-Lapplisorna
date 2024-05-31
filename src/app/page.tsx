5cdc09906290c4e2d77de88641375ee238b98e87
MartinAhlsen <martin.ahlsen@gmail.com>
2024-05-31 11:21:07 +0200

    remove test divs from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,6 @@ export default function Home() {
   return (
     <>
       <main>Parking Time</main>
-      <div className="h1">Hej</div>
-      <div className="h2">Hej</div>
-      <div className="h3">Hej</div>
-      <div className="h4">Hej</div>
-      <div className="h5">Hej</div>
-      <div className="h6">Hej</div>
-      <div className="button-text">Hej</div>
-      <div className="p1">Hej</div>
-      <div className="p2">Hej</div>
-      <div className="p3">Hej</div>
     </>
   );
 }
