7f2fd5b3c561cf7865d4607dabefc2feda8176ce
Davide Baldi <d.baldi88@gmail.com>
2024-06-01 15:58:33 +0200

    saving some progress

--- src/app/components/FAQ/index.tsx
+++ src/app/components/FAQ/index.tsx
@@ -2,30 +2,31 @@
 
 import React from "react";
 import {Accordion, AccordionItem} from "@nextui-org/react";
+import styles from "./FAQ.module.css"
 
-const FAQ=()=> {
-  const defaultContent:string =
+const FAQ = () => {
+  const defaultContent: string =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
     <div>
-    <Accordion selectionMode="multiple">
-      <AccordionItem key="1" aria-label="Accordion 1" title="What is Parking Time?">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="How do I use Parking Time?">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Is it free?">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="4" aria-label="Accordion 4" title="How do I get it in my municipality?">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="5" aria-label="Accordion 5" title="Known problems with the app">
-        {defaultContent}
-      </AccordionItem>
-    </Accordion>
+      <Accordion className={styles.accordionContainer} selectionMode="multiple">
+        <AccordionItem className={styles.accordion} key="1" aria-label="Accordion 1" title="What is Parking Time?">
+          <div className={styles.accordionContent}>{defaultContent}</div>
+        </AccordionItem>
+        <AccordionItem className={styles.accordion} key="2" aria-label="Accordion 2" title="How do I use Parking Time?">
+          <div className={styles.accordionContent}>{defaultContent}</div>
+        </AccordionItem>
+        <AccordionItem className={styles.accordion} key="3" aria-label="Accordion 3" title="Is it free?">
+          <div className={styles.accordionContent}>{defaultContent}</div>
+        </AccordionItem>
+        <AccordionItem className={styles.accordion} key="4" aria-label="Accordion 4" title="How do I get it in my municipality?">
+          <div className={styles.accordionContent}>{defaultContent}</div>
+        </AccordionItem>
+        <AccordionItem className={styles.accordion} key="5" aria-label="Accordion 5" title="Known problems with the app">
+          <div className={styles.accordionContent}>{defaultContent}</div>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
