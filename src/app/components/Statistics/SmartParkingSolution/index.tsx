8dca40ec87a65303b7648d1f1d4069ab8a498570
Davide Baldi <d.baldi88@gmail.com>
2024-06-03 08:09:54 +0200

    save prgoress-styling done image fixed, need to create subcomponent and pass inside

--- src/app/components/Statistics/SmartParkingSolution/index.tsx
+++ src/app/components/Statistics/SmartParkingSolution/index.tsx
@@ -3,15 +3,18 @@ import SmartParkingImg from "../../../../../public/Images/smartParkingSolution-h
 
 const SmartParkingSolution = () => {
     return (
-        <div className="h-[80vh] flex flex-col items-center md:flex-row">
-            <Image
+        <div className="h-[881px] md:h-[80vh] flex flex-col items-center md:flex-row">
+            <Image className="mt-10 mb-10"
       src={SmartParkingImg}
       width={340}
       height={340}
       alt=""
     />
-    <div className="flex flex-col ">
-        <h1>TEXT HERE</h1>
+    <div className="flex flex-col align items-center m-4">
+        <p className="h4 pb-8">Smart Parking</p>
+        <p className="p2">The parking disc has long been a tool to promote circulation and activity in city centers. It's a fantastic invention that benefits residents, visitors, and local businesses alike. We want to be part of this evolution by creating a free digital parking disc.</p>
+
+        <p className="p2">We're excited to share more about our solution in a meeting.</p>
     </div>
           
         </div>
