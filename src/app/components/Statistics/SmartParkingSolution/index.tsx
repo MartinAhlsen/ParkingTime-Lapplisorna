d41c9976db390f3213c1b6bde8e356e404fc8ae5
Davide Baldi <d.baldi88@gmail.com>
2024-06-03 09:01:41 +0200

    destop version done

--- src/app/components/Statistics/SmartParkingSolution/index.tsx
+++ src/app/components/Statistics/SmartParkingSolution/index.tsx
@@ -4,14 +4,14 @@ import ArrowTextSubComponent from "./ArrowTextSubComponent";
 
 const SmartParkingSolution = () => {
     return (
-        <div className="h-[881px] md:h-[80vh] flex flex-col items-center md:flex-row">
-            <Image className="mt-10 mb-10"
+        <div className="h-[881px] md:h-[80vh] flex flex-col items-center md:flex-row ">
+            <Image className="mt-10 mb-10  md:basis-1/2"
       src={SmartParkingImg}
-      width={340}
-      height={340}
+      width={800}
+      height={800}
       alt=""
     />
-    <div className="flex flex-col align items-center m-4">
+    <div className="flex flex-col items-center m-4 md:basis-1/2 md:items-start">
         <p className="h4 pb-8">Smart Parking</p>
         <p className="p2">The parking disc has long been a tool to promote circulation and activity in city centers. It's a fantastic invention that benefits residents, visitors, and local businesses alike. We want to be part of this evolution by creating a free digital parking disc.</p>
         <div className="mt-6 mb-6">
