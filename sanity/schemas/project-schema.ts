1fadb20b2ca4fcceb6bb8dee54dea57967e57b8a
Davide Baldi <d.baldi88@gmail.com>
2024-06-04 18:36:17 +0200

    project schema completed

---
+++ sanity/schemas/project-schema.ts
@@ -0,0 +1,49 @@
+import { sources } from "next/dist/compiled/webpack/webpack";
+import { title } from "process";
+
+const project = {
+    name: "project",
+    title: "Projects",
+    type: "Document",
+    fields: [{
+        name:"name",
+        title: "Name",
+        type: "string"
+    },
+    {
+        name:"slug",
+        title: "Slug",
+        type: "slug", //inbuild type in QROQ
+        options: {source:"name"}
+    },
+    {
+        name:"image",
+        title: "Image",
+        type: "image", //inbuild type in QROQ
+        options: {hotspot:true},
+        fields: [ 
+            {
+            name: "alt",
+            title: "Alt",
+            type: "string"
+        }
+        ]
+    },
+    {
+        name:"url",
+        title: "URL",
+        type: "url", //inbuild type in QROQ
+        
+    },
+    {
+        name:"content",
+        title: "Content",
+        type: "array", //inbuild type in QROQ
+        of: [{type: "block"}]
+    },
+       
+
+    ]
+}
+
+export default project
