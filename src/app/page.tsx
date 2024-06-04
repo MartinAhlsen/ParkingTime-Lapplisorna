54c424dd2e4b6c8e35174ba4f238f5f4df64fbb0
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-04 10:12:02 +0200

    remove typography examples and move button to app/components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "client component";
 
-import Button from "@/components/Button";
+import Button from "@/app/components/Button";
 
 interface ButtonProperties {
   text: string;
@@ -21,20 +21,6 @@ export default function Home() {
     <>
       <main>Parking Time</main>
       <Button {...TestButton} />
-      <div className="h1">Lorem ipsum dolor sit amet.</div>
-      <div className="h2">Lorem ipsum dolor sit amet.</div>
-      <div className="h3">Lorem ipsum dolor sit amet.</div>
-      <div className="h4">Lorem ipsum dolor sit amet.</div>
-      <div className="h5">Lorem ipsum dolor sit amet.</div>
-      <div className="h6">Lorem ipsum dolor sit amet.</div>
-      <div className="h7">Lorem ipsum dolor sit amet.</div>
-      <div className="button-text">Lorem ipsum dolor sit amet.</div>
-      <div className="p1">Lorem ipsum dolor sit amet.</div>
-      <div className="links">Lorem ipsum dolor sit amet.</div>
-      <div className="p2">Lorem ipsum dolor sit amet.</div>
-      <div className="p3">Lorem ipsum dolor sit amet.</div>
-      <div className="p3Italic">Lorem ipsum dolor sit amet.</div>
-      <div className="p3Quote">Lorem ipsum dolor sit amet.</div>
     </>
   );
 }
