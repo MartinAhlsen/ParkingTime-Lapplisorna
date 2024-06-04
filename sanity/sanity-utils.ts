fd7930dc385f5cdbc45cf02bd8985d730b3d83c1
Davide Baldi <d.baldi88@gmail.com>
2024-06-04 19:13:39 +0200

    min 48 connected to page and displayed

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,13 +1,20 @@
-
-
+import { createClient, groq } from "next-sanity";
 
 export async function getProjects() {
-
     const client = createClient({
-
-        basePath: '/studio',
         projectId: "m838832i",
         dataset: "production",
-  
     });
+
+    return client.fetch(
+        groq`*[_type == "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { usePathname } from 'next/navigation';
 import Header from './components/header';
 import Button from './components/Button';
 
+import { getProjects } from '../../sanity/sanity-utils';
+import project from '../../sanity/schemas/project-schema';
+
 interface ButtonProperties {
   text: string;
   url: string;
@@ -18,15 +21,22 @@ const TestButton: ButtonProperties = {
   colorTheme: "dark",
 };
 
-const App = () => {
+const App = async () => {
   const pathname = usePathname();
-
+  const projects = await getProjects();
   return (
     <div>
       <Header />
       <main>Parking Time</main>
       <Button {...TestButton} />
+      <div>
+        {projects.map((project)=> (
+          <div key={project._id}>{project.name}</div>
 
+        ))}
+
+
+      </div>
       <div className="h1">Lorem ipsum dolor sit amet.</div>
       <div className="h2">Lorem ipsum dolor sit amet.</div>
       <div className="h3">Lorem ipsum dolor sit amet.</div>
