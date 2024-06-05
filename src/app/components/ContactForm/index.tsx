122d0f22e492b9379ba127e2842363c621f56e9b
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-05 14:38:57 +0200

    wip

--- src/app/components/ContactForm/index.tsx
+++ src/app/components/ContactForm/index.tsx
@@ -3,14 +3,20 @@ import Button from "../Button";
 
 interface ContactFormData {
   name: string;
+  jobTitle: string;
   email: string;
+  phoneNumber: string;
+  reasonForContact: string;
   message: string;
 }
 
-const ContactForm: React.FC = () => {
+const ContactForm = () => {
   const [formData, setFormData] = useState<ContactFormData>({
     name: "",
+    jobTitle: "",
     email: "",
+    phoneNumber: "",
+    reasonForContact: "",
     message: "",
   });
 
@@ -47,7 +53,14 @@ const ContactForm: React.FC = () => {
       console.log("Form data submitted:", formData);
       setSubmitted(true);
       // Reset form
-      setFormData({ name: "", email: "", message: "" });
+      setFormData({
+        name: "",
+        jobTitle: "",
+        email: "",
+        phoneNumber: "",
+        reasonForContact: "",
+        message: "",
+      });
       setErrors({});
     }
   };
@@ -61,49 +74,98 @@ const ContactForm: React.FC = () => {
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2" htmlFor="name">
-            Name
+            First and last name
           </label>
           <input
             type="text"
             id="name"
             name="name"
-            value={formData.name}
+            placeholder="First and last name"
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            className="w-full px-3 py-2 border border-pt-primary rounded-md p2"
           />
           {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
         </div>
+        <div className="wrapper flex flex-col md:flex-row md:gap-3">
+          <div className="mb-4 p1">
+            <label className="block text-gray-700 mb-2 p1" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="ex. name@email.com"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-pt-primary rounded-md p2"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </div>
+          <div className="mb-4 p1">
+            <label className="block text-gray-700 mb-2 p1" htmlFor="email">
+              Phone Number
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="+46708123456"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-pt-primary rounded-md p2"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </div>
+        </div>
         <div className="mb-4">
-          <label className="block text-gray-700 mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email}</p>
-          )}
+          <select name="reasonForContact" id="reasonForContact">
+            <option className="h7" value="choose">
+              Choose from
+            </option>
+            <option className="h7" value="newClient">
+              New Client
+            </option>
+            <option className="h7" value="support">
+              Support
+            </option>
+            <option className="h7" value="feedback">
+              Feedback
+            </option>
+          </select>
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2" htmlFor="message">
-            Message
+            Write us a message
           </label>
           <textarea
             id="message"
             name="message"
+            placeholder="Type your message..."
             value={formData.message}
             onChange={handleChange}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            className="w-full px-3 py-2 border border-pt-primary rounded-md p2"
             rows={5}
           />
           {errors.message && (
             <p className="text-red-500 text-sm">{errors.message}</p>
           )}
+        </div>
+        <div className="mb-4">
+          <input
+            required
+            className="mr-2"
+            type="checkbox"
+            id="terms"
+            name="terms"
+          />
+          <label htmlFor="terms">
+            I accept the <a className="underline">Terms</a>{" "}
+          </label>
         </div>
         <Button text={"Send message"} type={"submit"} colorTheme="dark" />
       </form>
--- src/app/contact_us/page.tsx
+++ src/app/contact_us/page.tsx
@@ -1,18 +1,18 @@
+"use client";
+
 import TitleSubtitle from "../components/TitleSubtitle";
 import ContactForm from "../components/ContactForm";
 
 const Contact_us = () => {
   return (
     <>
-      <div className="contactUs">
+      <div className="contactUs bg-pt-gray2">
         <TitleSubtitle
           title="Contact us"
           subtitle="Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible."
         />
       </div>
-      <h2>Contact us</h2>
       <ContactForm />
-      <p>Hej</p>
     </>
   );
 };
