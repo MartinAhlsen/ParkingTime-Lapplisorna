baac64160349edba3c0eaf49a4a64c956189749c
Davide Baldi <d.baldi88@gmail.com>
2024-06-06 06:36:42 +0200

    image displayed

--- src/app/components/Articles/index.tsx
+++ src/app/components/Articles/index.tsx
@@ -1,22 +1,56 @@
-"use client"
-import React from 'react';
-import { usePathname } from "next/navigation";
-import { getProjects } from "../../../../sanity/sanity-utils";
-import TitleSubtitle from "../TitleSubtitle";
+"use client";
 
+import React, { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { getProjects } from '../../../../sanity/sanity-utils';
+import TitleSubtitle from '../TitleSubtitle';
+import Image, { StaticImageData } from 'next/image';
+interface Project {
+  image: StaticImageData;
+  id: string;
+  name: string;
+  
+  // Add other project properties as needed
+}
 
-const Articles = async() => {
-    const pathname = usePathname();
-    const projects = await getProjects();
+const Articles: React.FC = () => {
+  const pathname = usePathname();
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const fetchedProjects = await getProjects();
+      setProjects(fetchedProjects);
+    };
+
+    fetchProjects();
+  }, []);
+
     return (
         <div className="min-h-[1000px] bg-pt-grey2">
-          <TitleSubtitle
-            title="Don´t miss"
-            subtitle="We are expanding rapidly, subscribe to our newsletter."
-            overtitle="News"
-          />
+            <TitleSubtitle
+                title="Don’t miss"
+                subtitle="We are expanding rapidly, subscribe to our newsletter."
+                overtitle="News"
+            />
+            {/* You can render the projects here if needed */}
+            <div>
+                {projects.map(project => (
+                    <div key={project.id}>{project.name}  
+                    <Image
+                        src={project.image}
+                        width={100}
+                    height={100}
+      alt="Picture of the author"
+    />
+                    
+                    </div>
+                    
+
+                ))}
+            </div>
         </div>
-    )
+    );
 };
 
-export default Articles
+export default Articles;
