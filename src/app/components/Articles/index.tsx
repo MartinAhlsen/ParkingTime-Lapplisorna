55e295d0c5dbe46134d59b8a1dbfe2bc0f75f6c7
Davide Baldi <d.baldi88@gmail.com>
2024-06-06 07:11:46 +0200

    all 3 article displayed-NEED TO FIX CONTENT AND STYLE

--- src/app/components/Articles/index.tsx
+++ src/app/components/Articles/index.tsx
@@ -5,18 +5,22 @@ import { usePathname } from 'next/navigation';
 import { getProjects } from '../../../../sanity/sanity-utils';
 import TitleSubtitle from '../TitleSubtitle';
 import Image, { StaticImageData } from 'next/image';
+import { PortableTextBlock } from 'next-sanity';
+
+
 interface Project {
   image: StaticImageData;
   id: string;
   name: string;
-  
-  // Add other project properties as needed
+  alt: string;
+  content: PortableTextBlock[];//type for arra?????
+
 }
 
 const Articles: React.FC = () => {
   const pathname = usePathname();
   const [projects, setProjects] = useState<Project[]>([]);
-
+//DAVIDE-CHnage in normal dat fetching???
   useEffect(() => {
     const fetchProjects = async () => {
       const fetchedProjects = await getProjects();
@@ -34,15 +38,19 @@ const Articles: React.FC = () => {
                 overtitle="News"
             />
             {/* You can render the projects here if needed */}
-            <div>
+            <div className="flex  flex-col md:flex-row justify-evenly bg-pt-grey2">
                 {projects.map(project => (
-                    <div key={project.id}>{project.name}  
-                    <Image
+                    <div className="flex bg-white flex-col" key={project.id}>
+                        
+                        <p className='h6'>{project.name} </p>
+                        <Image
                         src={project.image}
-                        width={100}
-                    height={100}
-      alt="Picture of the author"
-    />
+                        width={300}
+                        height={300}
+                        alt={project.name}
+                        />
+                        <p>{project.alt}</p>
+                        <p className='links'>Read more</p>
                     
                     </div>
                     
