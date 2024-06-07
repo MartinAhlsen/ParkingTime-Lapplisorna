904ec84cb026031827976a22b39e998fbba91999
Davide Baldi <d.baldi88@gmail.com>
2024-06-07 14:45:58 +0200

    1 step forward

--- src/app/components/MatildaCEO/index.tsx
+++ src/app/components/MatildaCEO/index.tsx
@@ -1,25 +1,71 @@
-import Image from "next/image";
-import MatildaImg from "../../../../public/Images/Matilda-image.png"
+"use client"
 
-const MatildaCEO = () => {
-    return (
-        <div className="h-[840px] md:h-[494px] bg-pt-primary flex flex-col text-center text-white items-center p-6 md:flex-row	">
-            <div className="flex flex-col basis-1/2 items-center">
-                <Image className="rounded-full p-8"
-                    src={MatildaImg}
-                    width={250}
-                    height={250}
-                    alt="Picture of the author"
-                />
-                <p className="h5">Matilda Öhman, CEO</p>
-                <p className="h6">Matilda@email.123</p>
-            </div>
-            <div className="flex flex-col basis-1/2 items-start">
-                <p className="h4 pb-10 pt-6 md:pt-0 text-left">Want to know more?</p>
-                <p className="p2 text-left">Contact us to find out more about our solution and how we can implement it in your municipality.</p>
-            </div>
-        </div>
-    )
+import React from "react";
+import Image from "next/image";
+import MatildaImg from "../../../../public/Images/Matilda-image.png";
+import "./styles.css";
+import { motion, Variants } from "framer-motion";
+
+interface Props {
+  emoji: string;
+}
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 300
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
 };
 
-export default MatildaCEO
+const Card: React.FC<Props> = ({ emoji }) => {
+  const background = `linear-gradient(306deg, hsl(20, 100%, 50%), hsl(40, 100%, 50%))`;
+
+  return (
+    <motion.div
+      className="card-container"
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <div className="splash" style={{ background }} />
+      <motion.div className="card" variants={cardVariants}>
+        {emoji}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+const MatildaCEO: React.FC = () => {
+  return (
+    <div className="h-[840px] md:h-[494px] bg-pt-primary flex flex-col text-center text-white items-center p-6 md:flex-row">
+      <Card emoji={"🍊"} />
+
+      <div className="flex flex-col basis-1/2 items-center">
+        <Image
+          className="rounded-full p-8"
+          src={MatildaImg}
+          width={250}
+          height={250}
+          alt="Picture of the author"
+        />
+        <p className="h5">Matilda Öhman, CEO</p>
+        <p className="h6">Matilda@email.123</p>
+      </div>
+
+      <div className="flex flex-col basis-1/2 items-start">
+        <p className="h4 pb-10 pt-6 md:pt-0 text-left">Want to know more?</p>
+        <p className="p2 text-left">Contact us to find out more about our solution and how we can implement it in your municipality.</p>
+      </div>
+    </div>
+  );
+};
+
+export default MatildaCEO;
