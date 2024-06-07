07f46f46158ed1bfedd0d9421e2ee838941e45a5
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-07 09:01:36 +0200

    removed provisional test button and parking time text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
 import { AnimatePresence } from "framer-motion";
-import React from 'react';
-import { usePathname } from 'next/navigation';
-import Header from './components/header';
-import Button from './components/Button';
+import React from "react";
+import { usePathname } from "next/navigation";
+import Header from "./components/header";
+import Button from "./components/Button";
 
 interface ButtonProperties {
   text: string;
@@ -19,31 +19,32 @@ const TestButton: ButtonProperties = {
   url: "/se/news",
   colorTheme: "dark",
 };
-const App=()=> {
+const App = () => {
   const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     (async () => {
-      const LocomotiveScroll = (await import('locomotive-scroll')).default
-      const locomotiveScroll = new LocomotiveScroll()
-      
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
       setTimeout(() => {
-        setIsLoading(false)
-        document.body.style.cursor = 'default'
-        window.scrollTo(0, 0)
-      }, 2000)
-    })()
-  }, [])
-  return (<div>
-    <AnimatePresence mode="wait">
-          {isLoading && <SplashScreen />}
-        </AnimatePresence>
-    
-        <Header />
-      <main>Parking Time</main>
-      <Button {...TestButton} />
-    
-    </div>);
-}
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+  return (
+    <div>
+      <AnimatePresence mode="wait">
+        {isLoading && <SplashScreen />}
+      </AnimatePresence>
+
+      <Header />
+      {/* <main>Parking Time</main> */}
+      {/* <Button {...TestButton} /> */}
+    </div>
+  );
+};
 
 export default App;
