fa35be30ea90b8e79bf9ddc91d981cd4c00aaa96
Davide Baldi <d.baldi88@gmail.com>
2024-06-10 14:07:32 +0200

    removed emoji to accordions

--- src/app/components/FAQ/index.tsx
+++ src/app/components/FAQ/index.tsx
@@ -16,19 +16,19 @@ const FAQ = () => {
   return (
     <div>
       <Accordion className="bg-[#ECF2F1] p-5 flex flex-col justify-center items-center" >
-        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="1" aria-label="Accordion 1" title="What is Parking Time?" indicator={"❤️"}>
+        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="1" aria-label="Accordion 1" title="What is Parking Time?" >
           <div className="font-normal">{defaultContent1}</div>
         </AccordionItem>
-        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="2" aria-label="Accordion 2" title="How do I use Parking Time?" indicator={"🧡"}>
+        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="2" aria-label="Accordion 2" title="How do I use Parking Time?" >
           <div className="font-normal">{defaultContent2}</div>
         </AccordionItem>
-        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="3" aria-label="Accordion 3" title="Is it free?" indicator={"💛"}>
+        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="3" aria-label="Accordion 3" title="Is it free?" >
           <div className="font-normal">{defaultContent3}</div>
         </AccordionItem>
-        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="4" aria-label="Accordion 4" title="How do I get it in my municipality?" indicator={"💚"}>
+        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="4" aria-label="Accordion 4" title="How do I get it in my municipality?">
           <div className="font-normal">{defaultContent4}</div>
         </AccordionItem>
-        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="5" aria-label="Accordion 5" title="Known problems with the app" indicator={"💙"}>
+        <AccordionItem className="bg-white border h7 border-pt-lightgrayborder rounded-xl mb-2.5 transition-colors duration-300 ease-in-out flex flex-col  p-2.5 gap-2.5 text-lg font-bold w-[300px] md:w-[500px]" key="5" aria-label="Accordion 5" title="Known problems with the app" >
           <div className="font-normal">{defaultContent5}</div>
         </AccordionItem>
       </Accordion>
