8d8596d2aefbacd0b8a9cef52464b5b989d629af
Davide Baldi <d.baldi88@gmail.com>
2024-06-11 18:34:29 +0200

    integreting parking time

--- src/components/WhyParkingTime/index.tsx
+++ src/components/WhyParkingTime/index.tsx
@@ -2,20 +2,23 @@ import WhyCard from "./WhyCard";
 import arrowG from "../../../public/Images/arrowGrowth-icon.png";
 import phone from "../../../public/Images/phoneTap-icon.png"
 import group from "../../../public/Images/group-icon.png"
-import computer from "../../../public/Images/download-icon.png"; 
+import computer from "../../../public/Images/download-icon.png";  {/*FIND CORRECT ONE  */}
+import { useTranslations } from "next-intl";
+
 
 
 
 
 const WhyParkingTime = () => {
+    const t = useTranslations("why_parking_time");
     return (
         <div className="bg-pt-primary min-h-screen flex flex-col">
-            <p className="h3 text-center text-white p-6 leading-tight">Why Parking Time?</p>
+            <p className="h3 text-center text-white p-6 leading-tight">{t("title")}</p>
             <div className="bg-pt-primary min-h-screen flex flex-col md:grid md:grid-cols-2">
-          <WhyCard imageLink={arrowG} title="Increased revenue & reduced stress" paragraph="Our digital solution attracts more visitors by eliminating physical discs and reducing fines. This simplifies parking, boosts foot traffic, economic activity and city vibrancy  while reducing stress."/>
-          <WhyCard imageLink={phone} title="Simplified parking for users" paragraph="Our app lets you effortlessly set, adjust, and monitor parking time. With a user-friendly interface, you can quickly find spots and manage parking. We continuously update based on customer feedback to ensure an efficient and hassle-free experience."/>
-          <WhyCard imageLink={group} title="Integration with existing systems" paragraph="Our system for parking attendants integrates seamlessly with your existing setup. A unified system benefits you by streamlining operations and enhancing the experience for personnel by eliminating the need for two different systems and devices. We make this integration efficient, intuitive, and simple!"/>
-          <WhyCard imageLink={computer} title="Data-driven urban planning" paragraph="The digital parking disc can eventually optimize parking resources and urban planning through valuable usage data and behavior patterns, leading to a safer society with better control and access to parking spaces."/>
+          <WhyCard imageLink={arrowG} title={t("Comp_1_title")} paragraph={t("Comp_1_paragraph")} />
+          <WhyCard imageLink={phone} title={t("Comp_2_title")} paragraph={t("Comp_2_paragraph")}/>
+          <WhyCard imageLink={group} title={t("Comp_3_title")} paragraph={t("Comp_3_paragraph")}/>
+          <WhyCard imageLink={computer} title={t("Comp_4_title")} paragraph={t("Comp_4_paragraph")}/>
           </div>
         </div>
     )
