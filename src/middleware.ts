9238398de5713f2a8218a82c8032df075a465fdd
Davide Baldi <d.baldi88@gmail.com>
2024-06-11 18:24:51 +0200

    fixer

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: ['en', 'sv'],
  
   // Used when no locale matches
   defaultLocale: 'en'
