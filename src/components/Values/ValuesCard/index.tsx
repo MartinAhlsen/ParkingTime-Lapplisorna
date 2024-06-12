17f5cd9a0282b88e077939f6b81fae5e84395041
Davide Baldi <d.baldi88@gmail.com>
2024-06-12 17:05:33 +0200

    enough for minim available product

--- src/components/Values/ValuesCard/index.tsx
+++ src/components/Values/ValuesCard/index.tsx
@@ -11,18 +11,20 @@ interface ValuesCardProps {
 
 const ValuesCard = ({imageLink, title, paragraph}:ValuesCardProps) => {
     return (
-        <div className="flex flex-col p-4 m-4 md:m-8 md:p-8">
-            <div className="bg-white flex items-center">
+        <div className="flex flex-col p-4 m-4 md:m-8 md:p-8 items-center flex-basis-1/3">
+            <div className="bg-white flex items-center w-[95px] mb-6 rounded-[18px] border-pt-lightgrayborder border-2">
             <Image src={imageLink} 
-                width={50} 
-                height={50} 
+            className="items-center "
+                objectFit="cover"
+                width={250} 
+                height={250} 
                 alt={title} 
                 />
 
             </div>
             
-            <p className="p4 bg-pt-background">{title}</p>
-            <p className="p2 bg-pt-background">{paragraph}</p>
+            <p className="p4 bg-pt-background pb-6 w-[197px] text-center">{title}</p>
+            <p className="p2 bg-pt-background text-center">{paragraph}</p>
           
         </div>
     )
--- src/components/Values/index.tsx
+++ src/components/Values/index.tsx
@@ -1,16 +1,26 @@
 import { useTranslations } from "next-intl";
 import TitleSubtitle from "../TitleSubtitle";
+import ValuesCard from "./ValuesCard";
+import Simplicity from "../../../public/Images/values/simplicity-icon.png";
+import Innovation from "../../../public/Images/values/innovation-icon.png"
+import Friendly from "../../../public/Images/values/userFriendly-icon.png"
 
 
 const Values = () => {
     const t = useTranslations("about_us")
     return (
-        <div className="flex flex-col md:flex-row bg-pt-background">
+        <div className="flex flex-col  bg-pt-background items-center">
           <TitleSubtitle
         title={t("values_title")}
         subtitle={t("values_subtitle")}
         overtitle={null}
       />
+      <div className="flex flex-col md:flex-row sm:place-content-evenly px-6">
+        <ValuesCard imageLink={Simplicity} title={t("card_1_title")} paragraph={t("card_1_p")} />
+        <ValuesCard imageLink={Innovation} title={t("card_2_title")} paragraph={t("card_2_p")} />
+        <ValuesCard imageLink={Friendly} title={t("card_3_title")} paragraph={t("card_3_p")} />
+      </div>
+      
         </div>
     )
 };
