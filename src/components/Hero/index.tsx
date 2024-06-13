0ded01e6f4daf37ef14ed2f944f207bb9b90a38c
Davide Baldi <d.baldi88@gmail.com>
2024-06-13 09:29:27 +0200

    hero internazionlized

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import Button from "../Button";
 
 interface BannerProps {
@@ -24,6 +25,7 @@ const DownloadButton: ButtonProperties = {
 
 const Banner: React.FC<BannerProps> = ({ page }) => {
   let src, alt, imageClass, bannerContent, greenBar;
+  const t = useTranslations("hero_home")
 
   switch (page) {
     case 'home':
@@ -32,26 +34,26 @@ const Banner: React.FC<BannerProps> = ({ page }) => {
       imageClass = 'h-screen object-cover md:h-[calc(100vh-80px)]';
       bannerContent = (
         <div className="absolute inset-0 flex flex-col items-start justify-center text-white text-left px-4 md:px-8 lg:px-20 xl:px-32" style={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
-          <h1 className="button-text text-4xl md:text-7xl font-bold mb-4">What time is it?</h1>
-          <h1 className="button-text text-4xl md:text-7xl font-bold mb-10">Parking time!</h1>
-          <p className="text-lg md:text-2xl">Digital parking disc - directly on your mobile.</p>
-          <p className="text-lg md:text-2xl mb-10">A winning concept in a new, digitalized format</p>
+          <h1 className="button-text text-4xl md:text-7xl font-bold mb-4">{t("text1")}</h1>
+          <h1 className="button-text text-4xl md:text-7xl font-bold mb-10">{t("text2")}</h1>
+          <p className="text-lg md:text-2xl">{t("text3")}</p>
+          <p className="text-lg md:text-2xl mb-10">{t("text4")}</p>
           <div className="flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-4">
             <Button {...ContactButton}/>
             <Button {...DownloadButton}/>
           </div>
           <div className="bg-pt-green bg-opacity-80 absolute inset-x-0 bottom-0 h-32 md:h-16 flex flex-wrap md:flex-nowrap justify-around md:justify-center items-center md:space-x-20 text-white p-4">
             <div className="w-1/2 md:w-auto text-center md:text-left mb-2 md:mb-0">
-              <p>• Easy to use</p>
+              <p>• {t("sub1")}</p>
             </div>
             <div className="w-1/2 md:w-auto text-center md:text-right mb-2 md:mb-0">
-              <p>• Free to download</p>
+              <p>• {t("sub2")}</p>
             </div>
             <div className="w-1/2 md:w-auto text-center md:text-left">
-              <p>• Avoid fines</p>
+              <p>• {t("sub3")}</p>
             </div>
             <div className="w-1/2 md:w-auto text-center md:text-right">
-              <p>• Reduced Stress</p>
+              <p>• {t("sub4")}</p>
             </div>
           </div>
               </div>
