372706d174b587dd4793db3e5f461cdcb0397b34
shirin faile <shirin.faile@hotmail.com>
2024-06-13 17:41:26 +0200

    fixed styling for ipad

--- src/components/Testimonial/TestimonialCard/index..tsx
+++ src/components/Testimonial/TestimonialCard/index..tsx
@@ -13,7 +13,7 @@ interface TestimonialCardProps {
 const TestimonialCard = ({imageMain, imageTestimonial, mainText, name, role}:TestimonialCardProps) => {
     
     return (
-        <div className='flex flex-col h-[550px] w-[315px] md:h-[500px] md:w-[500px] lg:h-[450px] rounded-2xl bg-white m-6'>
+        <div className='flex flex-col h-[550px] w-[315px] md:h-[550px] md:w-[500px] lg:h-[480px] rounded-2xl bg-white m-6'>
           <Image className="m-8"
             src={imageMain}
             width={100}
@@ -23,7 +23,7 @@ const TestimonialCard = ({imageMain, imageTestimonial, mainText, name, role}:Tes
           <p className="p3Quote p-6">{mainText}</p>
           <div className='flex flex-row'>
           <Image
-          className="rounded-full m-4 md:mx-4 md:m-0"
+          className="rounded-full m-4 md:mx-4 md:m-2"
             src={imageTestimonial}
             width={50}
             height={50}
