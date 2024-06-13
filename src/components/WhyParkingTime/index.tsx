6d72e38d9826cb22124b3affd3d2ef29764ece3b
Carl Åberg <caaberg99@gmail.com>
2024-06-13 15:37:44 +0200

    fixed one button for testing :)

--- src/components/WhyParkingTime/index.tsx
+++ src/components/WhyParkingTime/index.tsx
@@ -4,6 +4,7 @@ import phone from "../../../public/Images/phoneTap-icon.png"
 import group from "../../../public/Images/group-icon.png"
 import computer from "../../../public/Images/download-icon.png";  {/*FIND CORRECT ONE  */}
 import { useTranslations } from "next-intl";
+import Button from "../Button";
 
 
 
@@ -14,11 +15,18 @@ const WhyParkingTime = () => {
     return (
         <div className="bg-pt-primary min-h-screen flex flex-col">
             <p className="h3 text-center text-white p-6 leading-tight">{t("title")}</p>
-            <div className="bg-pt-primary min-h-screen flex flex-col md:grid md:grid-cols-2">
+            <div className="bg-pt-primary min-h-[60vh] flex flex-col md:grid md:grid-cols-2">
           <WhyCard imageLink={arrowG} title={t("Comp_1_title")} paragraph={t("Comp_1_paragraph")} />
           <WhyCard imageLink={phone} title={t("Comp_2_title")} paragraph={t("Comp_2_paragraph")}/>
           <WhyCard imageLink={group} title={t("Comp_3_title")} paragraph={t("Comp_3_paragraph")}/>
           <WhyCard imageLink={computer} title={t("Comp_4_title")} paragraph={t("Comp_4_paragraph")}/>
+          </div>
+          <div className="flex p-6 justify-center">
+            <Button
+            text={t("Button")}
+            colorTheme="light"
+            url={t("url")}
+            />
           </div>
         </div>
     )
