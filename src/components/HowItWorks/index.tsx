b9a3b9a1f4976924534a1cd00ea142498457fd7c
Carl Åberg <caaberg99@gmail.com>
2024-06-14 11:42:36 +0200

    styling fixed

--- src/components/HowItWorks/index.tsx
+++ src/components/HowItWorks/index.tsx
@@ -8,11 +8,11 @@ const HowItWorks: React.FC = () => {
      <div className="bg-pt-background min-h-[50vh]">
         <div className="h3 text-center">How it works</div>
         <div className="flex w-full mx-auto flex-col xl:flex-row container justify-evenly items-stretch">
-            <div className="bg-white mx-auto w-[380px] p-6 rounded-[20px]">
-                <div className="flex justify-between">
+            <div className="bg-white mx-auto w-[380px] p-6 rounded-[20px] relative">
+                <div className="pb-10">
                 <img src="/Images/pen-icon.png" alt="pen"
                 className="w-[48px] h-[48px]" />
-                <div className="text-pt-gray2 text-[100px] leading-none">01</div>
+                <div className="text-pt-gray2 text-[100px] leading-none font-bold absolute top-[-20px] right-[-15px]">01</div>
                 </div>
                 <div className="h5">Enter your registration number</div>
                 <div className="p2">Open the app, register with your car's registration number.</div>
@@ -21,11 +21,11 @@ const HowItWorks: React.FC = () => {
                 <img src="/Images/right-icon.png" alt="right arrow"
                 className="rotate-90 xl:rotate-0"/>
             </div>
-            <div className="bg-white w-[380px] mx-auto p-6 rounded-[20px]">
-            <div className="flex justify-between">
-                <img src="/Images/parking-icon.png" alt="parking icon"
+            <div className="bg-white mx-auto w-[380px] p-6 rounded-[20px] relative">
+                <div className="pb-10">
+                <img src="/Images/parking-icon.png" alt="parking"
                 className="w-[48px] h-[48px]" />
-                <div className="flex text-pt-gray2 text-[100px] leading-none">02</div>
+                <div className="text-pt-gray2 text-[100px] leading-none font-bold absolute top-[-20px] right-[-15px]">02</div>
                 </div>
                 <div className="h5">Find your desired parking location</div>
                 <div className="p2">Use the map, search function, or zone list to find your desired parking location</div>
@@ -34,11 +34,11 @@ const HowItWorks: React.FC = () => {
                 <img src="/Images/right-icon.png" alt="right arrow"
                 className="rotate-90 xl:rotate-0"/>
             </div>
-            <div className="bg-white w-[380px] mx-auto p-6 rounded-[20px]">
-            <div className="flex justify-between">
-                <img src="/Images/clock-icon.png" alt="clock icon"
+            <div className="bg-white mx-auto w-[380px] p-6 rounded-[20px] relative">
+                <div className="pb-10">
+                <img src="/Images/clock-icon.png" alt="clock"
                 className="w-[48px] h-[48px]" />
-                <div className="flex text-pt-gray2 text-[100px] leading-none">03</div>
+                <div className="text-pt-gray2 text-[100px] leading-none font-bold absolute top-[-20px] right-[-15px]">03</div>
                 </div>
                 <div className="h5">Start your digital parking disc</div>
                 <div  className="p2">Once you select your parking zone, you can review any applicable rules, start parking, and then simply close the app.</div>
