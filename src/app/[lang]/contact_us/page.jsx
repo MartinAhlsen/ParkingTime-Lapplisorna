789b0520c225078ba71fe8ee5cf8e64c02ee5cb7
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-17 10:05:58 +0200

    wip

--- src/app/[lang]/contact_us/page.jsx
+++ src/app/[lang]/contact_us/page.jsx
@@ -1,12 +1,12 @@
 import { getDictionary } from "../dictionaries";
 
-const Contact_us = async ({params: {lang}}) => {
-  const dict = await getDictionary(lang)
-    return (
-        <>
-          <h2>{dict.contact_us.title}</h2>
-        </>
-    )
+const Contact_us = async ({ params: { lang } }) => {
+  const dict = await getDictionary(lang);
+  return (
+    <>
+      <h2>{dict.contact_us.title}</h2>
+    </>
+  );
 };
 
-export default Contact_us
+export default Contact_us;
--- src/app/contact_us/page.tsx
+++ src/app/contact_us/page.tsx
@@ -11,8 +11,9 @@ const Contact_us = () => {
           title="Contact us"
           subtitle="Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible."
         />
+        <h2>Hej</h2>
+        <ContactForm />
       </div>
-      <ContactForm />
     </>
   );
 };
