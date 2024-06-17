8beccd538b5fcd975e61d0445d110b3ff103f328
Davide Baldi <d.baldi88@gmail.com>
2024-06-17 10:32:39 +0200

    redo for PR

--- src/components/MatildaCEO/index.tsx
+++ src/components/MatildaCEO/index.tsx
@@ -13,15 +13,20 @@ const MatildaCEO: React.FC = () => {
   const t = useTranslations("MatildaCEO")
   return (
     <div className="h-[840px] md:h-[494px] bg-pt-primary flex flex-col text-center text-white items-center p-6 md:flex-row ">
-      
-
       <div className="flex flex-col basis-1/2 items-center">
-      <StaffCard staffImg={MatildaImg} hueA={20} hueB={40} text1={t("text1")} text2={"Matilda@email.com"} />
-      </div>
+                <Image className="rounded-[100%] p-8"
+                    src={MatildaImg}
+                    width={250}
+                    height={200}
+                    alt="Picture of the author"
+                />
+                <p className="h5">{t("text1")}</p>
+                <p className="h6">{"Matilda@email.com"}</p>
+            </div>
 
       <div className="flex flex-col basis-1/2 items-start">
         <p className="h4 pb-10 pt-6 md:pt-0 text-left">{t("title")}</p>
-        <p className="p2 text-left">{t("paragraph")}</p>
+        <p className="p2 text-left w-[60%]">{t("paragraph")}</p>
       </div>
     </div>
   );
