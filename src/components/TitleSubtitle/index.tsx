0862870fb57a6abd49759577f5742215e9e4b32f
Davide Baldi <d.baldi88@gmail.com>
2024-06-18 11:53:31 +0200

    fixer

--- src/components/TitleSubtitle/index.tsx
+++ src/components/TitleSubtitle/index.tsx
@@ -7,10 +7,11 @@ interface TitleSubtitleProps {
     overtitle: string | null
 }
 
-const TitleSubtitle = ({title, subtitle}:TitleSubtitleProps) => {
+const TitleSubtitle = ({title, subtitle,overtitle}:TitleSubtitleProps) => {
 
     return (
         <div className="bg-pt-background text-center"> 
+            <p className="h7 p-3">{overtitle}</p>
           <p className="h3 p-5 leading-normal">{title}</p>                                                                                                   
           <p className="h7 p-5">{subtitle}</p> 
         </div>
