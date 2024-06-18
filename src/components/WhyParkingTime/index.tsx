26b9825f8f1e9fa353bef3aecef7f5ee1066ef30
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-18 13:12:12 +0200

    add extra wide button setting for why parkingtime

--- src/components/WhyParkingTime/index.tsx
+++ src/components/WhyParkingTime/index.tsx
@@ -10,6 +10,7 @@ interface ButtonProperties {
   text: string;
   url: string;
   colorTheme: "light" | "dark";
+  extraWide: boolean;
 }
 
 const WhyParkingTime = () => {
@@ -21,16 +22,35 @@ const WhyParkingTime = () => {
     text: b("learnmore"),
     url: `/${locale}/about_us`,
     colorTheme: "light",
+    extraWide: true,
   };
 
   return (
     <div className="bg-pt-primary min-h-screen flex flex-col items-center">
-      <p className="text-4xl font-bold text-center text-white p-10 leading-tight">{t("title")}</p>
+      <p className="text-4xl font-bold text-center text-white p-10 leading-tight">
+        {t("title")}
+      </p>
       <div className="bg-pt-primary min-h-screen grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2 p-4 md:p-10 lg:p-20">
-        <WhyCard imageLink={arrowG} title={t("Comp_1_title")} paragraph={t("Comp_1_paragraph")} />
-        <WhyCard imageLink={phone} title={t("Comp_2_title")} paragraph={t("Comp_2_paragraph")} />
-        <WhyCard imageLink={computer} title={t("Comp_3_title")} paragraph={t("Comp_3_paragraph")} />
-        <WhyCard imageLink={group} title={t("Comp_4_title")} paragraph={t("Comp_4_paragraph")} />
+        <WhyCard
+          imageLink={arrowG}
+          title={t("Comp_1_title")}
+          paragraph={t("Comp_1_paragraph")}
+        />
+        <WhyCard
+          imageLink={phone}
+          title={t("Comp_2_title")}
+          paragraph={t("Comp_2_paragraph")}
+        />
+        <WhyCard
+          imageLink={computer}
+          title={t("Comp_3_title")}
+          paragraph={t("Comp_3_paragraph")}
+        />
+        <WhyCard
+          imageLink={group}
+          title={t("Comp_4_title")}
+          paragraph={t("Comp_4_paragraph")}
+        />
       </div>
       <div className="py-4 mb-8">
         <Button {...AboutUsButton} />
@@ -40,4 +60,3 @@ const WhyParkingTime = () => {
 };
 
 export default WhyParkingTime;
-
