b0c484d20e525ccc3e997cb98caa5abe9341fb56
Davide Baldi <d.baldi88@gmail.com>
2024-06-19 19:28:08 +0200

    save

--- src/app/[locale]/news/[slug]/page.tsx
+++ src/app/[locale]/news/[slug]/page.tsx
@@ -1,17 +1,33 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 
 import Header from "../../../../components/header";
+import { getProjects } from "../../../../../sanity/sanity-utils";
+import Image, { StaticImageData } from 'next/image';
 
 
 
-
-const SingleNews = () => {
+const SingleNews = ({ params }: { params: { slug: string } }) => {
   
+  const [article, setArticle] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const fetchedProjects = await getProjects();
+      setArticle(fetchedProjects);
+    };
+
+    fetchProjects();
+  }, [params.slug]);
+
 
   return (
     <>
       <Header />
       
+                        <p className='h6 pt-4 pb-4'>{article.name} </p>
+                        <p className='p2 max-w-[300px]'>{article.text2}</p>
      
       
      
