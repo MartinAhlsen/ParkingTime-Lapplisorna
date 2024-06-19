0856da7a5d6743ca2d0b709c84d4d0823ecdadb3
shirin faile <shirin.faile@hotmail.com>
2024-06-19 17:12:08 +0200

    fixed styling on testimonialcards

--- src/components/Testimonial/TestimonialCard/index..tsx
+++ src/components/Testimonial/TestimonialCard/index..tsx
@@ -4,38 +4,30 @@ import React from "react";
 
 interface TestimonialCardProps {
     imageMain: StaticImageData,
-    imageTestimonial:StaticImageData,
+    imageTestimonial: StaticImageData,
     mainText: string, 
-    name:string, 
-    role:string
+    name: string, 
+    role: string
 }
 
-const TestimonialCard = ({imageMain, imageTestimonial, mainText, name, role}:TestimonialCardProps) => {
-    
+const TestimonialCard = ({ imageMain, mainText, name, role }: TestimonialCardProps) => {
     return (
-        <div className='flex flex-col h-[550px] md:h-[550px] md:w-[500px] lg:h-[480px] rounded-2xl bg-white m-6 sm:m-20 md:m-6'>
-          <Image className="m-8 sm:m-16 md:m-8"
-            src={imageMain}
-            width={100}
-            height={100}
-            alt="Picture of the author"
-            />
-          <p className="p3Quote p-6">{mainText}</p>
-          <div className='flex flex-row'>
-          <Image
-          className="rounded-full m-4 md:mx-4 md:m-2"
-            src={imageTestimonial}
-            width={50}
-            height={50}
-            alt="Picture of the author"
-            />
-            <div className='flex flex-col'>
-                <p className="links text-[#F85252]">{name}</p>
-                <p className="p2">{role}</p>
+        <div className='flex flex-col h-[400px] md:h-[450px] md:w-[500px] lg:h-[410px] rounded-2xl bg-white m-6 sm:m-20 md:m-6 shadow-lg'>
+            <Image className="m-8 sm:m-16 md:m-8"
+                src={imageMain}
+                width={100}
+                height={100}
+                alt="Picture of the author"
+                />
+                <div className="overflow-auto px-4 py-4 flex flex-col flex-grow justify-between">
+                    <p className="p3Quote">{mainText}</p>
+                    <div className="flex flex-col items-center mt-4">
+                        <p className="links text-[#F85252] text-lg font-semibold text-center max-w-full break-words">{name}</p>
+                        <p className="p2 text-center max-w-full break-words">{role}</p>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    )
-};
-
-export default TestimonialCard
+        )
+    };
+    
+    export default TestimonialCard;
