3b28268dfa7b1b10908036410f0f13570c4badd8
shirin faile <shirin.faile@hotmail.com>
2024-06-19 15:08:26 +0200

    changed icons from feedback

--- src/components/Values/ValuesCard/index.tsx
+++ src/components/Values/ValuesCard/index.tsx
@@ -10,20 +10,20 @@ interface ValuesCardProps {
 
 const ValuesCard = ({imageLink, title, paragraph}: ValuesCardProps) => {
     return (
-        <div className="flex flex-col p-2 md:p-20 items-center">
-            <div className="bg-white mb-2 md:mb-4 rounded-[18px] border-pt-lightgrayborder border-2">
+        <div className="flex flex-col p-2 md:p-16 items-center">
+            <div className="bg-white mb-4 md:mb-4 rounded-[18px] border-pt-lightgrayborder border-2">
                 <Image
                     src={imageLink}
                     className="object-cover rounded-lg"
-                    width={120}
-                    height={120}
+                    width={140}
+                    height={140}
                     alt={title}
                 />
             </div>
             
-            <div className="text-center max-w-48">
-                <p className="bg-pt-background pb-2 text-lg font-bold">{title}</p>
-                <p className="bg-pt-background text-sm md:text-base">{paragraph}</p>
+            <div className="text-center max-w-48 pb-4">
+                <p className="bg-pt-background pb-2 text-2xl font-bold">{title}</p>
+                <p className="bg-pt-background md:text-base">{paragraph}</p>
             </div>
         </div>
     );
