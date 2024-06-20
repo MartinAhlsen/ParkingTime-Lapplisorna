d974083fc40552980fd4cd3c287d30d9f4a6193f
Davide Baldi <d.baldi88@gmail.com>
2024-06-20 11:49:25 +0200

    working fine

--- src/app/[locale]/news/[slug]/page.tsx
+++ src/app/[locale]/news/[slug]/page.tsx
@@ -31,19 +31,61 @@ const SingleNews = ({ params }: { params: { slug: string } }) => {
     <>
       <Header />
       
-                        
+              <div className="flex flex-col items-center p-8 grow">         
                         
      
-      {locale==="sv" &&
-      <p className='p2 max-w-[300px]'>{article.text_Short_sv}</p>
+              {locale==="sv" &&
+      <>
+      <div className="flex flex-row items-center justify-start">
+      <Image
+                        className='rounded-full aspect-square object-cover border-black border-[1px] mr-2 '
+                        src={article.image_author}
+                        width={50}
+                        height={50}
+                        alt={article.title_sv}
+                        />
+      <p className='p1 max-w-[300px] items-center	'>{article.author}, {article.date}</p>
+      </div>
       
+      <p className='h4 max-w-[300px] md:max-w-[700px] py-6'>{article.title_sv}</p>
+      <Image
+                        className='rounded-xl md:w-[500px] md:h-[300px] lg:w-[700px] lg:h-[650px]'
+                        src={article.image_article}
+                        width={300}
+                        height={300}
+                        alt={article.title_sv}
+                        />
+      <p className='p2 max-w-[300px] md:max-w-[700px] py-6'>{article.text_Long_sv}</p>
+      
+      </>
       }
 
       {locale!=="sv" &&
+      <>
+      <div className="flex flex-row items-center justify-start">
+      <Image
+                        className='rounded-full aspect-square object-cover border-black border-[1px] mr-2 '
+                        src={article.image_author}
+                        width={50}
+                        height={50}
+                        alt={article.title_en}
+                        />
+      <p className='p1 max-w-[300px] items-center	'>{article.author}, {article.date}</p>
+      </div>
       
-      <p className='p2 max-w-[300px]'>{article.text_Short_en}</p>
+      <p className='h4 max-w-[300px] md:max-w-[700px] py-6'>{article.title_en}</p>
+      <Image
+                        className='rounded-xl md:w-[500px] md:h-[300px] lg:w-[700px] lg:h-[650px]'
+                        src={article.image_article}
+                        width={300}
+                        height={300}
+                        alt={article.title_en}
+                        />
+      <p className='p2 max-w-[300px] md:max-w-[700px] py-6'>{article.text_Long_en}</p>
+      
+      </>
       }
-     
+     </div> 
     </>
   );
 };
