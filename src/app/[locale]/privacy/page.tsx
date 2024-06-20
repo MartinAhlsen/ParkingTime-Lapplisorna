0b73f8ca89d9282d1a4ca9330d02438901df9b43
Carl Åberg <caaberg99@gmail.com>
2024-06-20 03:30:45 +0200

    desperate attempt to make the animation work on both en and sv

--- src/app/[locale]/privacy/page.tsx
+++ src/app/[locale]/privacy/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../../../components/header";
+import Footer from "@/components/Footer";
 
 const Privacy = () => {
   return (
     <>
       <Header />
-      
+      <Footer/>
     </>
   );
 };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -14,11 +14,14 @@ interface ButtonProperties {
   colorTheme: "light" | "dark";
   formCategoryState?: string;
 }
- 
-
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -45,18 +48,20 @@ const Navbar = () => {
     open: { opacity: 1, x: 0 },
     closed: { opacity: 0, x: -20 },
   };
+
   const t = useTranslations("NavItem");
   const b = useTranslations("buttons");
-  const locale = useLocale(); 
+  const locale = useLocale();
   const ContactButton: ButtonProperties = {
-  text: b("contact_us"),
-  url: `/${locale}/contact_us`,
-  colorTheme: "light",
-};
+    text: b("contact_us"),
+    url: `/${locale}/contact_us`,
+    colorTheme: "light",
+  };
+
   return (
     <nav className="p-5 bg-pt-primary text-white items-center sticky top-0 z-50 flex justify-evenly grow" style={{height: '80px'}}>
       <div className="flex justify-between grow">
-        <Link href={`/${locale}/`}  className="flex items-center">
+        <Link href={`/${locale}/`} className="flex items-center">
           <Image
             src="/Images/P-icon.png"
             alt="Icon"
@@ -68,7 +73,7 @@ const Navbar = () => {
         </Link>
         <div className="hidden md:flex">
           <ul className="flex justify-end gap-[5%]">
-            <Link href={`/${locale}/#why-parking-time`}  className="flex items-center">
+            <Link href={`/${locale}/#why-parking-time`} className="flex items-center">
               <li className="hover:border-b button-text text-nowrap">{t("Why_parking_time?")}</li>
             </Link>
             <Link href={`/${locale}/about_us`} className="flex items-center">
@@ -80,10 +85,9 @@ const Navbar = () => {
             <Link href={`/${locale}/#faq`} className="flex items-center">
               <li className="hover:border-b button-text text-nowrap">{t("FAQ")}</li>
             </Link>
-            
+
             <Link href={`/${locale}/contact_us`} className="flex items-center">
-            
-              <Button {...ContactButton}/>
+              <Button {...ContactButton} />
             </Link>
           </ul>
           <LangToogle />
@@ -113,20 +117,18 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-      {isOpen && (
+      {isMounted && isOpen && ( // Ensure the menu is rendered only after mount
         <motion.div
           initial="closed"
           animate="open"
           variants={wrapperVariants}
           className="bg-pt-primary w-full md:hidden flex flex-col p-6 space-y-4 fixed top-[80px]"
         >
-          <motion.a variants={itemVariants} href={`/${locale}/#why-parking-time`} className="button-text" >Why Parking Time?</motion.a>
+          <motion.a variants={itemVariants} href={`/${locale}/#why-parking-time`} className="button-text">Why Parking Time?</motion.a>
           <motion.a variants={itemVariants} href={`/${locale}/about_us`} className="block mb-2 button-text">About Us</motion.a>
           <motion.a variants={itemVariants} href={`/${locale}/news`} className="block mb-2 button-text">News</motion.a>
-          <motion.a variants={itemVariants} href={`/${locale}/#faq`}className="block mb-2 button-text">FAQ</motion.a>
-          
+          <motion.a variants={itemVariants} href={`/${locale}/#faq`} className="block mb-2 button-text">FAQ</motion.a>
           <Button {...ContactButton} />
-          
         </motion.div>
       )}
     </nav>
