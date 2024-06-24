f883839082fb77974c3e8355fe737a0bbbcb824d
MartinAhlsen <martin.ahlsen@gmail.com>
2024-06-24 22:05:09 +0200

    change env

--- src/app/google-captcha-wrapper.tsx
+++ src/app/google-captcha-wrapper.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import React from "react";
+import 'dotenv/config'
 
 export default function GoogleCaptchaWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const recaptchaKey = process.env.RECAPTCHA_SECRET_KEY;
+  const recaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
   console.log("RECAPTCHA_SECRET_KEY" + recaptchaKey)
   if (!recaptchaKey) {
     console.error("reCAPTCHA site key is not defined");
